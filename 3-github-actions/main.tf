f38ea6ae1c4c4adaae3b9c4faf8206b75ab4ec89
Oscar Villacis <oscar.villacis@outlook.com>
2021-08-25 21:00:48 -0400

    my commit

--- 3-github-actions/main.tf
+++ 3-github-actions/main.tf
@@ -68,7 +68,7 @@ module "network" {
     costcenter  = "it"
     project     = "Area 51"
     owner       = "Oscar Villacis"
-    manager     = "Thomas Aderson"
+    manager     = "Thomas E. Aderson"
   }
 
   depends_on = [azurerm_resource_group.vnet]
